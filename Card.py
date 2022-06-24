c03de549635bc2c3510df3735bae9c552bf85652
koadegno <vinove9@hotmail.fr>
2022-06-24 12:35:23 +0200

    Doctstring and exception in card

--- Card.py
+++ Card.py
@@ -1,22 +1,32 @@
 
+from logging import raiseExceptions
+
+
 class Card:
 	""" Class representing Kuhn poker card
 		Thus only King ,Queen and Jack 
+
+		Attributes:
+			card_values : the values of the card
 	"""
 
 	values = ["Jack","Queen","King"]
 
-	def __init__(self,value) -> None:
-		self.value = value
+	def __init__(self,card_value) -> None:
+		
+		if card_value < 0 or card_value > 3:
+			raise TypeError("Card value must be between 0 and 2")
+
+		self.value = card_value
 	
 
-	def __lt__(self, card2):
-		if self.value < card2.value:
+	def __lt__(self, card_2):
+		if self.value < card_2.value:
 			return True
 		return False
 
-	def __gt__(self, c2):
-		if self.value > c2.value:
+	def __gt__(self, card_2):
+		if self.value > card_2.value:
 			return True
 		return False
 
